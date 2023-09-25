fd0e614dcbeef9c537a595dad47a8e33290c866b
DevExpressExampleBot <fakeEmail>
2023-09-25 13:07:01 +0400

    autogenerate VB

--- VB/CustomRowInGrid/BlankItem.vb
+++ VB/CustomRowInGrid/BlankItem.vb
@@ -1,7 +1,3 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
-Imports System.Text
 Imports System.ComponentModel
 Imports DevExpress.XtraGrid.Views.Grid
 Imports System.Windows.Forms
@@ -10,54 +6,55 @@ Imports DevExpress.Utils.Drawing
 Imports System.Drawing
 
 Namespace GridCustomRows
-	Friend Class BlankItem
-		Private ItemObject As PartContainer
-		Private Wrapper As DataSourceWrapper
-		Private View As GridView
-		Public Text As String
-		Public Sub New(ByVal wrapper As DataSourceWrapper, ByVal view As GridView, ByVal id As Integer)
-			ItemObject = New PartContainer()
-			ItemObject.ID = id
-			Me.Wrapper = wrapper
-			Me.Wrapper.CustomRows.Add(ItemObject)
-			Me.View = view
-			AddHandler Me.View.GridControl.Paint, AddressOf GridControl_Paint
-			AddHandler Me.View.ShowingEditor, AddressOf View_ShowingEditor
-			Text = "[Empty String]"
-		End Sub
-		Public Sub New(ByVal wrapper As DataSourceWrapper, ByVal view As GridView, ByVal id As Integer, ByVal text As String)
-			Me.New(wrapper, view,id)
-			Me.Text = text
-		End Sub
-		Private Function ItemRowHandle() As Integer
-			Dim index As Integer = Wrapper.IndexOf(ItemObject)
-			Dim nullHandle As Integer = View.GetRowHandle(View.GetRowHandle(index))
-			Return nullHandle
-		End Function
-		Private Sub GridControl_Paint(ByVal sender As Object, ByVal e As PaintEventArgs)
 
-			Dim gvi As GridViewInfo = TryCast(View.GetViewInfo(), GridViewInfo)
-			Dim gri As GridRowInfo = gvi.RowsInfo.FindRow(ItemRowHandle())
-			If gri Is Nothing Then
-				Return
-			End If
-			Dim cache As New GraphicsCache(e)
-			Dim drawRect As Rectangle = gri.DataBounds
-			drawRect.Height -= 1
+    Friend Class BlankItem
 
-			e.Graphics.FillRectangle(gri.Appearance.GetBackBrush(cache), drawRect)
-			drawRect.Inflate(-3, -3)
-			cache.DrawString(Text, gri.Appearance.Font, gri.Appearance.GetForeBrush(cache), drawRect, gri.Appearance.GetStringFormat())
-		End Sub
+        Private ItemObject As PartContainer
 
-		Private Sub View_ShowingEditor(ByVal sender As Object, ByVal e As CancelEventArgs)
-			If View.FocusedRowHandle = ItemRowHandle() Then
-				e.Cancel = True
-			End If
-		End Sub
+        Private Wrapper As DataSourceWrapper
 
+        Private View As GridView
 
+        Public Text As String
 
+        Public Sub New(ByVal wrapper As DataSourceWrapper, ByVal view As GridView, ByVal id As Integer)
+            ItemObject = New PartContainer()
+            ItemObject.ID = id
+            Me.Wrapper = wrapper
+            Me.Wrapper.CustomRows.Add(ItemObject)
+            Me.View = view
+            AddHandler Me.View.GridControl.Paint, New PaintEventHandler(AddressOf GridControl_Paint)
+            AddHandler Me.View.ShowingEditor, New CancelEventHandler(AddressOf View_ShowingEditor)
+            Text = "[Empty String]"
+        End Sub
 
-	End Class
+        Public Sub New(ByVal wrapper As DataSourceWrapper, ByVal view As GridView, ByVal id As Integer, ByVal text As String)
+            Me.New(wrapper, view, id)
+            Me.Text = text
+        End Sub
+
+        Private Function ItemRowHandle() As Integer
+            Dim index As Integer = Wrapper.IndexOf(ItemObject)
+            Dim nullHandle As Integer = View.GetRowHandle(View.GetRowHandle(index))
+            Return nullHandle
+        End Function
+
+        Private Sub GridControl_Paint(ByVal sender As Object, ByVal e As PaintEventArgs)
+            Dim gvi As GridViewInfo = TryCast(View.GetViewInfo(), GridViewInfo)
+            Dim gri As GridRowInfo = gvi.RowsInfo.FindRow(ItemRowHandle())
+            If gri Is Nothing Then Return
+            Dim cache As GraphicsCache = New GraphicsCache(e)
+            Dim drawRect As Rectangle = gri.DataBounds
+            drawRect.Height -= 1
+            e.Graphics.FillRectangle(gri.Appearance.GetBackBrush(cache), drawRect)
+            drawRect.Inflate(-3, -3)
+            cache.DrawString(Text, gri.Appearance.Font, gri.Appearance.GetForeBrush(cache), drawRect, gri.Appearance.GetStringFormat())
+        End Sub
+
+        Private Sub View_ShowingEditor(ByVal sender As Object, ByVal e As CancelEventArgs)
+            If View.FocusedRowHandle = ItemRowHandle() Then
+                e.Cancel = True
+            End If
+        End Sub
+    End Class
 End Namespace
--- VB/CustomRowInGrid/datasourceWrapper.vb
+++ VB/CustomRowInGrid/datasourceWrapper.vb
@@ -1,229 +1,229 @@
-﻿Imports Microsoft.VisualBasic
 Imports System
-Imports System.Collections.Generic
-Imports System.Text
 Imports System.ComponentModel
 
 Namespace GridCustomRows
-	Friend Class DataSourceWrapper
-		Implements IBindingList
-		Private _neastedList As IBindingList
-		Private _customRows As IBindingList
 
-		Public Property NestedList() As IBindingList
-			Get
-				Return _neastedList
-			End Get
-			Set(ByVal value As IBindingList)
-				If _neastedList Is value Then
-					Return
-				End If
-				_neastedList = value
-				RaiseEvent ListChanged(Me, New ListChangedEventArgs(ListChangedType.Reset, 0))
-			End Set
-		End Property
-		Public Property CustomRows() As IBindingList
-			Get
-				Return _customRows
-			End Get
-			Set(ByVal value As IBindingList)
-				If _customRows Is value Then
-					Return
-				End If
-				_customRows = value
-				RaiseEvent ListChanged(Me, New ListChangedEventArgs(ListChangedType.Reset, NestedList.Count))
-			End Set
-		End Property
+    Friend Class DataSourceWrapper
+        Implements IBindingList
 
-		Public Sub New(ByVal target As IBindingList, ByVal custom As IBindingList)
-			MyBase.New()
-			_neastedList = target
-			_customRows = custom
-			AddHandler NestedList.ListChanged, AddressOf NeastedListChanged
-			AddHandler CustomRows.ListChanged, AddressOf NeastedListChanged
-		End Sub
+        Private _neastedList As IBindingList
 
-		Private Sub NeastedListChanged(ByVal sender As Object, ByVal e As ListChangedEventArgs)
-			If ListChangedEvent IsNot Nothing Then
-				If sender Is NestedList Then
-					RaiseEvent ListChanged(Me, New ListChangedEventArgs(e.ListChangedType, e.NewIndex, e.OldIndex))
-				Else
-					RaiseEvent ListChanged(Me, New ListChangedEventArgs(e.ListChangedType, e.NewIndex + NestedList.Count, e.OldIndex + NestedList.Count))
-				End If
-			End If
-		End Sub
+        Private _customRows As IBindingList
 
-		Public Sub AddIndex(ByVal [property] As PropertyDescriptor) Implements IBindingList.AddIndex
-			Throw New NotImplementedException()
-		End Sub
+        Public Property NestedList As IBindingList
+            Get
+                Return _neastedList
+            End Get
 
-		Public Function AddNew() As Object Implements IBindingList.AddNew
-			Throw New NotImplementedException()
-		End Function
+            Set(ByVal value As IBindingList)
+                If _neastedList Is value Then Return
+                _neastedList = value
+                RaiseEvent ListChanged(Me, New ListChangedEventArgs(ListChangedType.Reset, 0))
+            End Set
+        End Property
 
-		Public ReadOnly Property AllowEdit() As Boolean Implements IBindingList.AllowEdit
-			Get
-				Return True
-			End Get
-		End Property
+        Public Property CustomRows As IBindingList
+            Get
+                Return _customRows
+            End Get
 
-		Public ReadOnly Property AllowNew() As Boolean Implements IBindingList.AllowNew
-			Get
-				Return True
-			End Get
-		End Property
+            Set(ByVal value As IBindingList)
+                If _customRows Is value Then Return
+                _customRows = value
+                RaiseEvent ListChanged(Me, New ListChangedEventArgs(ListChangedType.Reset, NestedList.Count))
+            End Set
+        End Property
 
-		Public ReadOnly Property AllowRemove() As Boolean Implements IBindingList.AllowRemove
-			Get
-				Throw New NotImplementedException()
-			End Get
-		End Property
+        Public Sub New(ByVal target As IBindingList, ByVal custom As IBindingList)
+            MyBase.New()
+            _neastedList = target
+            _customRows = custom
+            AddHandler NestedList.ListChanged, New ListChangedEventHandler(AddressOf NeastedListChanged)
+            AddHandler CustomRows.ListChanged, New ListChangedEventHandler(AddressOf NeastedListChanged)
+        End Sub
 
-		Public Sub ApplySort(ByVal [property] As PropertyDescriptor, ByVal direction As ListSortDirection) Implements IBindingList.ApplySort
-			Throw New NotImplementedException()
-		End Sub
+        Private Sub NeastedListChanged(ByVal sender As Object, ByVal e As ListChangedEventArgs)
+            If ListChangedEvent IsNot Nothing Then
+                If sender Is NestedList Then
+                    RaiseEvent ListChanged(Me, New ListChangedEventArgs(e.ListChangedType, e.NewIndex, e.OldIndex))
+                Else
+                    RaiseEvent ListChanged(Me, New ListChangedEventArgs(e.ListChangedType, e.NewIndex + NestedList.Count, e.OldIndex + NestedList.Count))
+                End If
+            End If
+        End Sub
 
-		Public Function Find(ByVal [property] As PropertyDescriptor, ByVal key As Object) As Integer Implements IBindingList.Find
-			Throw New NotImplementedException()
-		End Function
+        Public Sub AddIndex(ByVal [property] As PropertyDescriptor) Implements IBindingList.AddIndex
+            Throw New NotImplementedException()
+        End Sub
 
-		Public ReadOnly Property IsSorted() As Boolean Implements IBindingList.IsSorted
-			Get
-				Throw New NotImplementedException()
-			End Get
-		End Property
+        Public Function AddNew() As Object Implements IBindingList.AddNew
+            Throw New NotImplementedException()
+        End Function
 
-		Public Event ListChanged As ListChangedEventHandler Implements IBindingList.ListChanged
+        Public ReadOnly Property AllowEdit As Boolean Implements IBindingList.AllowEdit
+            Get
+                Return True
+            End Get
+        End Property
 
-		Public Sub RemoveIndex(ByVal [property] As PropertyDescriptor) Implements IBindingList.RemoveIndex
-			Throw New NotImplementedException()
-		End Sub
+        Public ReadOnly Property AllowNew As Boolean Implements IBindingList.AllowNew
+            Get
+                Return True
+            End Get
+        End Property
 
-		Public Sub RemoveSort() Implements IBindingList.RemoveSort
-			Throw New NotImplementedException()
-		End Sub
+        Public ReadOnly Property AllowRemove As Boolean Implements IBindingList.AllowRemove
+            Get
+                Throw New NotImplementedException()
+            End Get
+        End Property
 
-		Public ReadOnly Property SortDirection() As ListSortDirection Implements IBindingList.SortDirection
-			Get
-				Throw New NotImplementedException()
-			End Get
-		End Property
+        Public Sub ApplySort(ByVal [property] As PropertyDescriptor, ByVal direction As ListSortDirection) Implements IBindingList.ApplySort
+            Throw New NotImplementedException()
+        End Sub
 
-		Public ReadOnly Property SortProperty() As PropertyDescriptor Implements IBindingList.SortProperty
-			Get
-				Throw New NotImplementedException()
-			End Get
-		End Property
+        Public Function Find(ByVal [property] As PropertyDescriptor, ByVal key As Object) As Integer Implements IBindingList.Find
+            Throw New NotImplementedException()
+        End Function
 
-		Public ReadOnly Property SupportsChangeNotification() As Boolean Implements IBindingList.SupportsChangeNotification
-			Get
-				Return True
-			End Get
-		End Property
+        Public ReadOnly Property IsSorted As Boolean Implements IBindingList.IsSorted
+            Get
+                Throw New NotImplementedException()
+            End Get
+        End Property
 
-		Public ReadOnly Property SupportsSearching() As Boolean Implements IBindingList.SupportsSearching
-			Get
-				Throw New NotImplementedException()
-			End Get
-		End Property
+        Public Event ListChanged As ListChangedEventHandler Implements IBindingList.ListChanged
 
-		Public ReadOnly Property SupportsSorting() As Boolean Implements IBindingList.SupportsSorting
-			Get
-				Throw New NotImplementedException()
-			End Get
-		End Property
+        Public Sub RemoveIndex(ByVal [property] As PropertyDescriptor) Implements IBindingList.RemoveIndex
+            Throw New NotImplementedException()
+        End Sub
 
-		Public Function Add(ByVal value As Object) As Integer Implements System.Collections.IList.Add
-			Throw New NotImplementedException()
-		End Function
+        Public Sub RemoveSort() Implements IBindingList.RemoveSort
+            Throw New NotImplementedException()
+        End Sub
 
-		Public Sub Clear() Implements System.Collections.IList.Clear
-			Throw New NotImplementedException()
-		End Sub
+        Public ReadOnly Property SortDirection As ListSortDirection Implements IBindingList.SortDirection
+            Get
+                Throw New NotImplementedException()
+            End Get
+        End Property
 
-		Public Function Contains(ByVal value As Object) As Boolean Implements System.Collections.IList.Contains
-			Throw New NotImplementedException()
-		End Function
+        Public ReadOnly Property SortProperty As PropertyDescriptor Implements IBindingList.SortProperty
+            Get
+                Throw New NotImplementedException()
+            End Get
+        End Property
 
-		Public Function IndexOf(ByVal value As Object) As Integer Implements System.Collections.IList.IndexOf
+        Public ReadOnly Property SupportsChangeNotification As Boolean Implements IBindingList.SupportsChangeNotification
+            Get
+                Return True
+            End Get
+        End Property
 
-			Dim index As Integer = CustomRows.IndexOf(value)
-			If index = -1 Then
-				Return NestedList.IndexOf(value)
-			End If
-			Return index + NestedList.Count
-		End Function
+        Public ReadOnly Property SupportsSearching As Boolean Implements IBindingList.SupportsSearching
+            Get
+                Throw New NotImplementedException()
+            End Get
+        End Property
 
-		Public Sub Insert(ByVal index As Integer, ByVal value As Object) Implements System.Collections.IList.Insert
-			Throw New NotImplementedException()
-		End Sub
+        Public ReadOnly Property SupportsSorting As Boolean Implements IBindingList.SupportsSorting
+            Get
+                Throw New NotImplementedException()
+            End Get
+        End Property
 
-		Public ReadOnly Property IsFixedSize() As Boolean Implements System.Collections.IList.IsFixedSize
-			Get
-				Throw New NotImplementedException()
-			End Get
-		End Property
+        Public Function Add(ByVal value As Object) As Integer Implements Collections.IList.Add
+            Throw New NotImplementedException()
+        End Function
 
-		Public ReadOnly Property IsReadOnly() As Boolean Implements System.Collections.IList.IsReadOnly
-			Get
-				Throw New NotImplementedException()
-			End Get
-		End Property
+        Public Sub Clear() Implements Collections.IList.Clear
+            Throw New NotImplementedException()
+        End Sub
 
-		Public Sub Remove(ByVal value As Object) Implements System.Collections.IList.Remove
-			Throw New NotImplementedException()
-		End Sub
+        Public Function Contains(ByVal value As Object) As Boolean Implements Collections.IList.Contains
+            Throw New NotImplementedException()
+        End Function
 
-		Public Sub RemoveAt(ByVal index As Integer) Implements System.Collections.IList.RemoveAt
-			Throw New NotImplementedException()
-		End Sub
+        Public Function IndexOf(ByVal value As Object) As Integer Implements Collections.IList.IndexOf
+            Dim index As Integer = CustomRows.IndexOf(value)
+            If index = -1 Then Return NestedList.IndexOf(value)
+            Return index + NestedList.Count
+        End Function
 
-		Default Public Property Item(ByVal index As Integer) As Object Implements System.Collections.IList.Item
-			Get
-				If index >= NestedList.Count Then
-					Return CustomRows(index - NestedList.Count)
-				End If
-				Return NestedList(index)
-			End Get
-			Set(ByVal value As Object)
-				If index < CustomRows.Count Then
-					CustomRows(index) = value
-				End If
-				NestedList(index) = value
-			End Set
-		End Property
+        Public Sub Insert(ByVal index As Integer, ByVal value As Object) Implements Collections.IList.Insert
+            Throw New NotImplementedException()
+        End Sub
 
-		Public Sub CopyTo(ByVal array As Array, ByVal index As Integer) Implements System.Collections.ICollection.CopyTo
-			Throw New NotImplementedException()
-		End Sub
+        Public ReadOnly Property IsFixedSize As Boolean Implements Collections.IList.IsFixedSize
+            Get
+                Throw New NotImplementedException()
+            End Get
+        End Property
 
-		Public ReadOnly Property Count() As Integer Implements System.Collections.ICollection.Count
-			Get
-				Return NestedList.Count + CustomRows.Count
-			End Get
-		End Property
+        Public ReadOnly Property IsReadOnly As Boolean Implements Collections.IList.IsReadOnly
+            Get
+                Throw New NotImplementedException()
+            End Get
+        End Property
 
-		Public ReadOnly Property IsSynchronized() As Boolean Implements System.Collections.ICollection.IsSynchronized
-			Get
-				Throw New NotImplementedException()
-			End Get
-		End Property
+        Public Sub Remove(ByVal value As Object) Implements Collections.IList.Remove
+            Throw New NotImplementedException()
+        End Sub
 
-		Public ReadOnly Property SyncRoot() As Object Implements System.Collections.ICollection.SyncRoot
-			Get
-				Throw New NotImplementedException()
-			End Get
-		End Property
+        Public Sub RemoveAt(ByVal index As Integer) Implements Collections.IList.RemoveAt
+            Throw New NotImplementedException()
+        End Sub
 
-		Public Function GetEnumerator() As System.Collections.IEnumerator Implements System.Collections.IEnumerable.GetEnumerator
-			Throw New NotImplementedException()
-		End Function
-		Public Function IsCustomItem(ByVal value As Object) As Boolean
-			Return CustomRows.Contains(value)
-		End Function
-		Public Function IsCustomItemIndex(ByVal index As Integer) As Boolean
+        Default Public Property Item(ByVal index As Integer) As Object Implements Collections.IList.Item
+            Get
+                If index >= NestedList.Count Then
+                    Return CustomRows(index - NestedList.Count)
+                End If
 
-			Return ((index >= NestedList.Count) AndAlso (index < CustomRows.Count + NestedList.Count))
-		End Function
-	End Class
+                Return NestedList(index)
+            End Get
+
+            Set(ByVal value As Object)
+                If index < CustomRows.Count Then
+                    CustomRows(index) = value
+                End If
+
+                NestedList(index) = value
+            End Set
+        End Property
+
+        Public Sub CopyTo(ByVal array As Array, ByVal index As Integer) Implements Collections.ICollection.CopyTo
+            Throw New NotImplementedException()
+        End Sub
+
+        Public ReadOnly Property Count As Integer Implements Collections.ICollection.Count
+            Get
+                Return NestedList.Count + CustomRows.Count
+            End Get
+        End Property
+
+        Public ReadOnly Property IsSynchronized As Boolean Implements Collections.ICollection.IsSynchronized
+            Get
+                Throw New NotImplementedException()
+            End Get
+        End Property
+
+        Public ReadOnly Property SyncRoot As Object Implements Collections.ICollection.SyncRoot
+            Get
+                Throw New NotImplementedException()
+            End Get
+        End Property
+
+        Public Function GetEnumerator() As Collections.IEnumerator Implements Collections.IEnumerable.GetEnumerator
+            Throw New NotImplementedException()
+        End Function
+
+        Public Function IsCustomItem(ByVal value As Object) As Boolean
+            Return CustomRows.Contains(value)
+        End Function
+
+        Public Function IsCustomItemIndex(ByVal index As Integer) As Boolean
+            Return index >= NestedList.Count AndAlso index < CustomRows.Count + NestedList.Count
+        End Function
+    End Class
 End Namespace
